6ca20ed1bcbaccdfe6dbadb9752d13dabbd626c6
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-25 21:09:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
   end
 
   def is_valid?
-    (@side_1>0 && @side_2>0 && @side_3>0) && ((@side_1+@side_2 >= @side_3) && (@side_2+@side_3 >= @side_1) && (@side_1+@side_3 >= @side_2))
+    (@side_1>0 && @side_2>0 && @side_3>0) && ((@side_1+@side_2 > @side_3) && (@side_2+@side_3 > @side_1) && (@side_1+@side_3 > @side_2))
   end
 
   def is_isosceles?
